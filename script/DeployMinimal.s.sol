ac8ef8c2e993420ba8c0c4a07f851cb7fe7062eb
panditdhamdhere <dhamdherepandit@gmail.com>
2024-07-04 10:15:56 +0530

    helperconfig

--- script/DeployMinimal.s.sol
+++ script/DeployMinimal.s.sol
@@ -3,5 +3,10 @@ pragma solidity ^0.8.25;
 
 import {Script} from "forge-std/Script.sol";
 import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
-contract DeployMinial is Script {}
+contract DeployMinial is Script {
+    function run() public {}
+
+    function deployMinimalAccount() public {}
+}
