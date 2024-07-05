641aa4352ca955473b1561eeab122e4049a253fa
panditdhamdhere <dhamdherepandit@gmail.com>
2024-07-05 11:59:52 +0530

    testValidationOfUserOps passed

--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -100,5 +100,44 @@ contract MinimalAccountTest is Test {
         //Assert
         assertEq(actualSigner, minimalAccount.owner());
     }
-    // function testValidationOfUserOps() public {}
+
+    // 1. Sign user ops
+    // 2. Call validate user ops
+    // 3. Assert the return is correct
+
+    function testValidationOfUserOps() public {
+         //Arrange
+         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+         address dest = address(usdc);
+         uint256 value = 0;
+         bytes memory functionData = abi.encodeWithSelector(
+             ERC20Mock.mint.selector,
+             address(minimalAccount),
+             AMOUNT
+         );
+         bytes memory executeCalldata = abi.encodeWithSelector(
+             MinimalAccount.execute.selector,
+             dest,
+             value,
+             functionData
+         );
+         PackedUserOperation memory packedUserOp = sendPackedUserOp
+             .generateSignedUserOperation(
+                 executeCalldata,
+                 helperConfig.getConfig()
+             );
+         bytes32 userOperationHash = IEntryPoint(
+             helperConfig.getConfig().entryPoint
+         ).getUserOpHash(packedUserOp);
+         uint256 missingAccountFunds = 1e18;
+
+         // ACT
+         vm.prank(helperConfig.getConfig().entryPoint);
+         uint256 validationData = minimalAccount.validateUserOp(packedUserOp, userOperationHash, missingAccountFunds);
+
+         assertEq(validationData , 0);
+         // ASSERT
+    }
+
+    // function testEntryPointCanExecuteCommands() public {}
 }
