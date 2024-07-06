41e66dc5c7eb5fcce05d9453abad92e060cc4b39
panditdhamdhere <dhamdherepandit@gmail.com>
2024-07-06 14:09:08 +0530

    zkstarted

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -9,11 +9,49 @@ import {HelperConfig} from "script/HelperConfig.s.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
 
 contract SendPackedUserOp is Script {
     using MessageHashUtils for bytes32;
 
-    function run() public {}
+    function run() public {
+        // Setup
+        HelperConfig helperConfig = new HelperConfig();
+        address dest = helperConfig.getConfig().usdc; // arbitrum mainnet USDC address
+        uint256 value = 0;
+        address minimalAccountAddress = DevOpsTools.get_most_recent_deployment(
+            "MinimalAccount",
+            block.chainid
+        );
+
+        bytes memory functionData = abi.encodeWithSelector(
+            IERC20.approve.selector,
+            RANDOM_APPROVER,
+            1e18
+        );
+        bytes memory executeCalldata = abi.encodeWithSelector(
+            MinimalAccount.execute.selector,
+            dest,
+            value,
+            functionData
+        );
+        PackedUserOperation memory userOp = generateSignedUserOperation(
+            executeCalldata,
+            helperConfig.getConfig(),
+            minimalAccountAddress
+        );
+        PackedUserOperation[] memory ops = new PackedUserOperation[](1);
+        ops[0] = userOp;
+
+        // Send transaction
+        vm.startBroadcast();
+        IEntryPoint(helperConfig.getConfig().entryPoint).handleOps(
+            ops,
+            payable(helperConfig.getConfig().account)
+        );
+        vm.stopBroadcast();
+    }
 
     function generateSignedUserOperation(
         bytes memory callData,
